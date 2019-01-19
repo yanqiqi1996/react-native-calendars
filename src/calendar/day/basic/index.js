b92a2cc6736f38ba78fca4713a012f23a9af67b3
yanqiqi1996 <31203049+yanqiqi1996@users.noreply.github.com>
2019-01-19 10:31:09 +0800

    rewrite
    修改源码，使其周一和周日颜色为红色

--- src/calendar/day/basic/index.js
+++ src/calendar/day/basic/index.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   TouchableOpacity,
   Text,
   View
 } from 'react-native';
 import PropTypes from 'prop-types';
-import {shouldUpdate} from '../../../component-updater';
+import { shouldUpdate } from '../../../component-updater';
 
 import styleConstructor from './style';
 
@@ -56,24 +56,28 @@ class Day extends Component {
     if (marking.marked) {
       dotStyle.push(this.style.visibleDot);
       if (marking.dotColor) {
-        dotStyle.push({backgroundColor: marking.dotColor});
+        dotStyle.push({ backgroundColor: marking.dotColor });
       }
-      dot = (<View style={dotStyle}/>);
+      dot = (<View style={dotStyle} />);
     }
-
-    if (marking.selected) {
-      containerStyle.push(this.style.selected);
-      if (marking.selectedColor) {
-        containerStyle.push({backgroundColor: marking.selectedColor});
+    if (this.props.dayInWeek == 0 || this.props.dayInWeek == 6){  //我修改de源码
+      textStyle.push({
+        color:'#e46430'
+      })
+    }
+      if (marking.selected) {
+        containerStyle.push(this.style.selected);
+        if (marking.selectedColor) {
+          containerStyle.push({ backgroundColor: marking.selectedColor });
+        }
+        dotStyle.push(this.style.selectedDot);
+        textStyle.push(this.style.selectedText);
+      } else if (isDisabled) {
+        textStyle.push(this.style.disabledText);
+      } else if (this.props.state === 'today') {
+        containerStyle.push(this.style.today);
+        textStyle.push(this.style.todayText);
       }
-      dotStyle.push(this.style.selectedDot);
-      textStyle.push(this.style.selectedText);
-    } else if (isDisabled) {
-      textStyle.push(this.style.disabledText);
-    } else if (this.props.state === 'today') {
-      containerStyle.push(this.style.today);
-      textStyle.push(this.style.todayText);
-    }
 
     return (
       <TouchableOpacity
